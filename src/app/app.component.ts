58052307d668d0a99535fc890b19d0c21b2e515d
Javier Elizalde <javier.elizalde@globant.com>
2021-04-19 19:52:20 -0500

    Setup validations for first and last name, started logic to add diff rows for emails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Contact } from './models/contact.interface';
-import { CountryOptions, getCountryName } from './models/optionModel-countriesOptions';
-import { StateOptions } from './models/optionModel-statesOptions';
-import { FirebaseContactService } from './services/fb.contact.service';
+import { Component, OnInit } from '@angular/core'
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Observable } from 'rxjs'
+import { Contact } from './models/contact.interface'
+import { CountryOptions, getCountryName } from './models/optionModel-countriesOptions'
+import { StateOptions } from './models/optionModel-statesOptions'
+import { FirebaseContactService } from './services/fb.contact.service'
 
 @Component({
   selector: 'app-root',
@@ -16,8 +17,31 @@ export class AppComponent implements OnInit {
 
   countryOptions = CountryOptions;
   stateOptions = StateOptions;
+  newFormGroup: FormGroup
 
-  constructor(private fb_contact: FirebaseContactService) { }
+  get emails() {
+    return (this.newFormGroup.get('emails') as FormArray)['controls']
+  }
+
+  constructor(
+    private fb_contact: FirebaseContactService,
+    private formBuilder: FormBuilder
+    ) {
+      this.newFormGroup = this.formBuilder.group({
+        first_name: this.formBuilder.control('', [Validators.required]),
+        last_name: this.formBuilder.control('', [Validators.required]),
+        street: this.formBuilder.control(''),
+        city: this.formBuilder.control(''),
+        state: this.formBuilder.control(''),
+        country: this.formBuilder.control(''),
+        zip: this.formBuilder.control(''),
+        emails: this.formBuilder.array([
+          this.formBuilder.group({
+            email: this.formBuilder.control('', [Validators.email])
+          })
+        ]),
+      })
+     }
 
   ngOnInit() {
     this.getContacts()
@@ -35,4 +59,7 @@ export class AppComponent implements OnInit {
     return `${contact.street}, ${contact.city}, ${contact.state}, ${getCountryName(contact.country)}, ${contact.zip}`
   }
 
+  saveContact() {
+    console.log('this.newFormGroup.getRawValue', this.newFormGroup.getRawValue())
+  }
 }
